be37af88f6c2372ebfee8398c384ffe2568763ad
Kadek Arta <gsumiartawan@gmail.com>
2020-04-12 00:56:13 +0800

    Github Page

---
+++ build/precache-manifest.0363219cb04c86a6ac62243ac782ce4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd91d96affbe8900c73a74ec24b3a3f",
+    "url": "/corona/index.html"
+  },
+  {
+    "revision": "dc89cfb0e9a46cec8c11",
+    "url": "/corona/static/css/main.7778feb8.chunk.css"
+  },
+  {
+    "revision": "121cbc34b69fed5f7cdb",
+    "url": "/corona/static/js/2.a891affb.chunk.js"
+  },
+  {
+    "revision": "bfb19a4219c76289b554e6db627c1aec",
+    "url": "/corona/static/js/2.a891affb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc89cfb0e9a46cec8c11",
+    "url": "/corona/static/js/main.1adc3a0f.chunk.js"
+  },
+  {
+    "revision": "10563266cc97041c2017",
+    "url": "/corona/static/js/runtime-main.b4d4afbe.js"
+  }
+]);
